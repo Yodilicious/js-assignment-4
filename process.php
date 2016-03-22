56bf4de3aa61f597d0f99c16ab0acaa04b0ba8e7
Jodi Visser <visser.jodi@gmail.com>
2016-03-21 23:12:48 -0400

    Working on PHP

---
+++ process.php
@@ -0,0 +1,43 @@
+<?php
+    //Picture values
+	$sheepPicture = $_POST['quantitySheep'];
+    $roseatePicture = $_POST['quantityRoseate'];
+    $leopardPicture = $_POST['quantityLeopard'];
+    $elephantPicture = $_POST['quantityElephant'];
+    $chameleonPicture = $_POST['quantityChameleon'];
+    $lionCubPicture = $_POST['quantityLionCub'];
+    $lionsPicture = $_POST['quantityLions'];
+    $snowLionPicture = $_POST['quantitySnowLion'];
+    
+    //Form values
+    $firstName = $_POST['firstName'];
+    $lastName = $_POST['lastName'];
+    $address = $_POST['address'];
+    $city = $_POST['city'];
+    $postalCode = $_POST['postalCode'];
+    $province = $_POST['province'];
+    $country = $_POST['country'];
+    $emailAddress = $_POST['email'];
+    $phoneNumber = $_POST['phoneNumber'];
+    $payment = $_POST['payment'];
+    
+    print "$sheepPicture";
+    print "$roseatePicture";
+    print "$leopardPicture";
+    print "$elephantPicture";
+    print "$chameleonPicture";
+    print "$lionCubPicture";
+    print "$lionsPicture";
+    print "$snowLionPicture";
+    
+    print "$firstName";
+    print "$lastName";
+    print "$address"; 
+    print "$city";
+    print "$postalCode";
+    print "$province";
+    print "$country";
+    print "$emailAddress";
+    print "$phoneNumber";
+    print "$payment";
+?>
